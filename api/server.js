06f391b5e4cee4e0d014d9e59190ded51adc9106
David <davidellis1020@gmail.com>
2021-05-04 21:54:26 -0700

    User can be deleted, and changed

--- api/server.js
+++ api/server.js
@@ -5,6 +5,52 @@ const User = require('./users/model');
 const server = express();
 server.use(express.json())
 
+server.put('/api/users/:id', async (req, res) => {
+    try {
+        const possibleUser = await User.findById(req.params.id)
+        if (!possibleUser) {
+            res.status(404).json({
+                message: 'message: "The user with the specified ID does not exist'
+            })
+        } else {
+            if (!req.body.name || !req.body.bio) {
+                res.status(400).json({
+                    message: "Please provide name and bio for the user",
+                })
+            } else {
+                const updatedUser = await User.update(req.params.id, req.body)
+                res.status(200).json(updatedUser)
+            }
+        }
+    } catch (err) {
+        res.status(500).json({
+            message: 'error updating user',
+            err: err.message,
+            stack: err.stack,
+        })
+    }
+})
+
+server.delete('/api/users/:id', async (req, res) => {
+    try {
+        const possibleUser = await User.findById(req.params.id)
+        if(!possibleUser) {
+            res.status(404).json({
+                message: 'message: The user with the specified ID does not exist'
+            })
+        }else {
+            const deletedUser = await User.remove(possibleUser.id)
+            res.status(200).json(deletedUser)
+        }
+    } catch (err) {
+        res.status(500).json({
+            message: 'error deleting user',
+            err: err.message,
+            stack: err.stack,
+        })
+    }
+});
+
 server.post('/api/users', (req, res) => {
     const user = req.body;
     if (!user.name || !user.bio) {
@@ -24,7 +70,7 @@ server.post('/api/users', (req, res) => {
             })
         })
     }
-})
+});
 
 server.get('/api/users', (req, res) => {
     User.find()
@@ -64,6 +110,6 @@ server.use('*', (req, res) => {
     res.status(404).json({
         message: 'not found'
     })
-})
+});
 
 module.exports = server; // EXPORT YOUR SERVER instead of {}
